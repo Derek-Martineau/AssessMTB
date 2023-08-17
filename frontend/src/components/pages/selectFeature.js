3a04138bfe52d26f93c77d84495e497b8f45a05b
Derek Martineau <derekmartineau@hotmail.com>
2023-08-16 22:33:28 -0400

    initial line selection

--- frontend/src/components/pages/selectFeature.js
+++ frontend/src/components/pages/selectFeature.js
@@ -1,63 +1,83 @@
 import React, { Component } from "react";
 import { Card, Button } from "react-bootstrap";
-import { Navigate } from "react-router-dom"; 
+import { Navigate } from "react-router-dom";
 
 class CallFeature extends Component {
   constructor() {
     super();
     this.state = {
       data: [],
-      selectedPark: null,
-      redirectToSegment: false,
+      selectedLine: null,
+      activeButtonIndex: -1,
+      redirectToFeat2: false,
     };
   }
 
-  async componentDidMount() {
-    const response = await fetch(`http://localhost:8081/parks/trailparks`);
-    const json = await response.json();
-    this.setState({ data: json });
-  }
-
-  handleParkSelection = (park) => {
-    this.setState({ selectedPark: park });
+  handleLineSelection = (line, index) => {
+    this.setState({ selectedLine: line, activeButtonIndex: index });
   };
 
   handleNextClick = () => {
-    const { selectedPark } = this.state;
-    if (selectedPark) {
-      // Do something with the selected park, e.g., store it in the state or pass it to a parent component.
-      console.log("Selected Park:", selectedPark);
-      this.setState({ redirectToSegment: true });
+    const { selectedLine } = this.state;
+    if (selectedLine) {
+      console.log("Selected Line:", selectedLine);
+      this.setState({ redirectToFeat2: true });
     } else {
-      alert("Please select a park first.");
+      alert("Please select a line first.");
     }
   };
 
   render() {
-    const buttonStyle = {
-      margin: "3px 0", // Adjust the vertical padding
-    };
-
     const buttonWrapperStyle = {
       display: "flex",
       justifyContent: "space-between",
       flexDirection: "row-reverse",
     };
 
-    const { redirectToSegment } = this.state;
+    const { redirectToFeat2, activeButtonIndex } = this.state;
 
-    if (redirectToSegment) {
+    if (redirectToFeat2) {
       return <Navigate to="/newAssessment/selectSegment" />;
     }
 
     return (
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "150vh" }}>
-        <Card style={{ width: "45rem"}}>
+        <Card style={{ width: "45rem" }}>
           <Card.Body>
-            <Card.Title>Which Line did you choose?</Card.Title>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <Card.Title>Which line did you choose?</Card.Title>
+            </div>
             <div style={buttonWrapperStyle}>
-              <Button variant="primary" onClick={this.handleNextClick} disabled={!this.state.selectedPark}>
+              <Button
+                variant="primary"
+                onClick={this.handleNextClick}
+                disabled={activeButtonIndex === -1}
+              >
                 Next
+              </Button>
+              <Button
+                variant={activeButtonIndex === 0 ? "success" : "primary"} // Apply "success" variant for the selected button
+                onClick={() => this.handleLineSelection("Walked", 0)}
+              >
+                Walked
+              </Button>
+              <Button
+                variant={activeButtonIndex === 1 ? "success" : "primary"}
+                onClick={() => this.handleLineSelection("C", 1)}
+              >
+                C
+              </Button>
+              <Button
+                variant={activeButtonIndex === 2 ? "success" : "primary"}
+                onClick={() => this.handleLineSelection("B", 2)}
+              >
+                B
+              </Button>
+              <Button
+                variant={activeButtonIndex === 3 ? "success" : "primary"}
+                onClick={() => this.handleLineSelection("A", 3)}
+              >
+                A
               </Button>
             </div>
           </Card.Body>
