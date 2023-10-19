e60f3a52e8006326444e22c7b6933426eb7e6646
Derek-Martineau <derekmartineau@hotmail.com>
2023-10-19 13:19:42 -0400

    frontend(chore) saving line to state

--- frontend/src/components/pages/selectFeature.js
+++ frontend/src/components/pages/selectFeature.js
@@ -7,12 +7,11 @@ class CallFeature extends Component {
     super();
     this.state = {
       data: [],
-      selectedLine: null,
-      activeButtonIndex: -1,
+      selectedLines: Array(6).fill(null), // Store selected lines for each photo
       redirectToFeat2: false,
       redirectToNewAssessment: false,
       showModal: false,
-      photos: [], // Array to store photos associated with the segment
+      photos: [],
       photoFilePaths: [
         "/images/Gordon_Walls_F1.JPG",
         "/images/Gordon_Walls_F2.JPG",
@@ -20,18 +19,17 @@ class CallFeature extends Component {
         "/images/Gordon_Walls_F4.JPG",
         "/images/Gordon_Walls_F5.JPG",
         "/images/Gordon_Walls_F6.JPG",
-      ],      
-      currentPhotoIndex: 0, // Added property to keep track of the currently displayed photo
+      ],
+      currentPhotoIndex: 0,
     };
   }
 
   componentDidMount() {
-    // Fetch photos associated with the segment here
     this.fetchPhotosForSegment();
   }
 
   fetchPhotosForSegment = async () => {
-    const { segmentId } = this.props.match.params; // Assuming you receive segmentId via route params
+    const { segmentId } = this.props.match.params;
 
     try {
       const response = await fetch(`http://localhost:8081/api/segments/getphotos/${segmentId}`);
@@ -40,20 +38,64 @@ class CallFeature extends Component {
     } catch (error) {
       console.error("Error fetching photos:", error);
     }
+  }
+
+  // Create a mapping object for line values
+  lineValues = {
+    "A": 8,
+    "B": 6,
+    "C": 4,
+    "Walked": 1,
   };
 
   handleLineSelection = (line, index) => {
-    this.setState({ selectedLine: line, activeButtonIndex: index });
+    const { selectedLines, currentPhotoIndex } = this.state;
+
+    // Use lineValues to get the value for the selected line
+    const lineValue = this.lineValues[line];
+
+    // Update the selectedLines array for the current photo with the line value
+    selectedLines[currentPhotoIndex] = lineValue;
+
+    this.setState({ selectedLines }, () => {
+      console.log("Selected Lines:", this.state.selectedLines);
+    });
   };
 
   handleNextClick = () => {
-    const { selectedLine } = this.state;
-    if (selectedLine) {
-      console.log("Selected Line:", selectedLine);
-      this.setState({ redirectToFeat2: true });
+    const { selectedLines, currentPhotoIndex, photoFilePaths } = this.state;
+
+    // Use lineValues to get the value for the selected line
+    const lineValue = selectedLines[currentPhotoIndex];
+
+    if (lineValue !== null) {
+      this.storeSelectedLine(lineValue);
+
+      if (currentPhotoIndex < photoFilePaths.length - 1) {
+        this.setState((prevState) => ({
+          currentPhotoIndex: prevState.currentPhotoIndex + 1,
+        }));
+      } else {
+        this.saveResultToMongoDB().then(() => {
+          this.setState({ redirectToFeat2: true });
+        });
+      }
     } else {
       alert("Please select a line first.");
     }
+  };
+
+  saveResultToMongoDB = async () => {
+    console.log("Simulating saving result to MongoDB...");
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 2000);
+    });
+  };
+
+  storeSelectedLine = (selectedLine) => {
+    console.log(`Storing selected line in the database: ${selectedLine}`);
   };
 
   handleDiscardClick = () => {
@@ -77,18 +119,14 @@ class CallFeature extends Component {
   };
 
   handleNextPhoto = () => {
-    // Increment the currentPhotoIndex, and loop back to the first photo if at the end
     this.setState((prevState) => ({
-      currentPhotoIndex:
-        (prevState.currentPhotoIndex + 1) % this.state.photoFilePaths.length,
+      currentPhotoIndex: (prevState.currentPhotoIndex + 1) % this.state.photoFilePaths.length,
     }));
   };
 
   handlePreviousPhoto = () => {
-    // Decrement the currentPhotoIndex, and loop to the last photo if at the beginning
     this.setState((prevState) => ({
-      currentPhotoIndex:
-        (prevState.currentPhotoIndex - 1 + this.state.photoFilePaths.length) %
+      currentPhotoIndex: (prevState.currentPhotoIndex - 1 + this.state.photoFilePaths.length) %
         this.state.photoFilePaths.length,
     }));
   };
@@ -100,7 +138,7 @@ class CallFeature extends Component {
       flexDirection: "row-reverse",
     };
 
-    const { redirectToFeat2, activeButtonIndex, redirectToNewAssessment, showModal, showInstructionsModal } = this.state;
+    const { redirectToFeat2, selectedLines, currentPhotoIndex, redirectToNewAssessment, showModal, showInstructionsModal } = this.state;
 
     if (redirectToFeat2) {
       return <Navigate to="/results" />;
@@ -111,31 +149,19 @@ class CallFeature extends Component {
     }
 
     return (
-      <div style={{ background: '#5A5A5A', minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center",}}>
+      <div style={{ background: '#5A5A5A', minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
         <Card style={{ width: "45rem" }}>
           <Card.Body>
-            {/* Display the currently selected photo */}
             <div>
               <h3>Photo for Segment</h3>
               <div>
-              <img
-                key={this.state.currentPhotoIndex}
-                src={this.state.photoFilePaths[this.state.currentPhotoIndex]}
-                alt={`Photo ${this.state.currentPhotoIndex + 1}`}
-                style={{ maxWidth: "100%", maxHeight: "100%" }}
-              />
-
+                <img
+                  key={currentPhotoIndex}
+                  src={this.state.photoFilePaths[currentPhotoIndex]}
+                  alt={`Photo ${currentPhotoIndex + 1}`}
+                  style={{ maxWidth: "100%", maxHeight: "100%" }}
+                />
               </div>
-            </div>
-            
-            {/* Add Next and Previous buttons for photo navigation */}
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <Button variant="secondary" onClick={this.handlePreviousPhoto}>
-                Previous
-              </Button>
-              <Button variant="secondary" onClick={this.handleNextPhoto}>
-                Next
-              </Button>
             </div>
             <div style={{ display: "flex", justifyContent: "center" }}>
               <Card.Title>Which line did you choose?</Card.Title>
@@ -143,26 +169,26 @@ class CallFeature extends Component {
             <br />
             <div style={buttonWrapperStyle}>
               <Button
-                variant={activeButtonIndex === 0 ? "success" : "primary"}
-                onClick={() => this.handleLineSelection("Walked", 0)}
+                variant={selectedLines[currentPhotoIndex] === 1 ? "success" : "primary"}
+                onClick={() => this.handleLineSelection("Walked", currentPhotoIndex)}
               >
                 Walked
               </Button>
               <Button
-                variant={activeButtonIndex === 1 ? "success" : "primary"}
-                onClick={() => this.handleLineSelection("C", 1)}
+                variant={selectedLines[currentPhotoIndex] === 4 ? "success" : "primary"}
+                onClick={() => this.handleLineSelection("C", currentPhotoIndex)}
               >
                 C
               </Button>
               <Button
-                variant={activeButtonIndex === 2 ? "success" : "primary"}
-                onClick={() => this.handleLineSelection("B", 2)}
+                variant={selectedLines[currentPhotoIndex] === 6 ? "success" : "primary"}
+                onClick={() => this.handleLineSelection("B", currentPhotoIndex)}
               >
                 B
               </Button>
               <Button
-                variant={activeButtonIndex === 3 ? "success" : "primary"}
-                onClick={() => this.handleLineSelection("A", 3)}
+                variant={selectedLines[currentPhotoIndex] === 8 ? "success" : "primary"}
+                onClick={() => this.handleLineSelection("A", currentPhotoIndex)}
               >
                 A
               </Button>
@@ -174,14 +200,13 @@ class CallFeature extends Component {
               <Button
                 variant="primary"
                 onClick={this.handleNextClick}
-                disabled={activeButtonIndex === -1}
+                disabled={selectedLines[currentPhotoIndex] === null}
               >
                 Next
               </Button>
             </div>
           </Card.Body>
         </Card>
-        {/* Alert Modal  */}
         <Modal show={showModal} onHide={this.handleModalCancel}>
           <Modal.Header closeButton>
             <Modal.Title>Change Segment Confirmation</Modal.Title>
@@ -194,7 +219,6 @@ class CallFeature extends Component {
             <Button variant="danger" onClick={this.handleModalConfirm}>Confirm</Button>
           </Modal.Footer>
         </Modal>
-        {/*Instruction Modal*/}
         <Modal show={showInstructionsModal} onHide={this.handleInstructionsClose}>
           <Modal.Header closeButton>
             <Modal.Title>Assessment Instructions</Modal.Title>
@@ -202,8 +226,8 @@ class CallFeature extends Component {
           <Modal.Body>
             <p><strong>Step 1:</strong> Select the park you would like to assess a segment in. Press the "Next" button to confirm and continue.</p>
             <p><strong>Step 2:</strong> Select the segment you would like to assess your technical ability on.</p>
-            <p><strong>Step 3:</strong> Follow through the features displayed. Select the line that you traveled over. Lines are ranked in alphapbetical order descending. This means that the A line is the hardest line to complete and results in the most points possible per feature. You can also select walked if you had failed/missed the feature.</p>
-            <p><strong>Step 4:</strong> View your assessment score and decide to delete or share the segment. The Assessment score can also be saved to only your personal records if you wish not to share the results.</p>
+            <p><strong>Step 3:</strong> Follow through the features displayed. Select the line that you traveled over. Lines are ranked in alphapbetical order descending. This means that the A line is the hardest line to complete and results in the most points possible per feature. You can also select "Walked" if you had failed/missed the feature.</p>
+            <p><strong>Step 4:</strong> View your assessment score and decide to delete or share the segment. The assessment score can also be saved to only your personal records if you wish not to share the results.</p>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={this.handleInstructionsClose}>Close</Button>
