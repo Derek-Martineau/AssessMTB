ae27deb7d6ef5d7085857c6a73102ab5d472434d
Derek-Martineau <derekmartineau@hotmail.com>
2023-10-24 14:31:33 -0400

    Frontend(feat): Created a enter moving time form that will be used to calculate the assessment Score

--- frontend/src/components/pages/assessResultPage.js
+++ frontend/src/components/pages/assessResultPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, Button, Modal } from "react-bootstrap";
+import { Card, Button, Modal, Form } from "react-bootstrap";
 import { Navigate } from "react-router-dom";
 
 class PostResults extends Component {
@@ -8,6 +8,8 @@ class PostResults extends Component {
     this.state = {
       data: [],
       selectedLine: null,
+      movingTime: "", // Add a state for moving time
+      confirmedMovingTime: "", // Add a state to store confirmed moving time
       activeButtonIndex: -1,
       redirectToFeat2: false,
       redirectToWillowdale: false,
@@ -15,27 +17,40 @@ class PostResults extends Component {
     };
   }
 
-  handleNextClick = async () => {
-    const { selectedLine } = this.state;
-    if (selectedLine) {
-      try {
-        // Post the results to the server
-        const response = await fetch("http://localhost:8081/results", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ result: selectedLine }),
-        });
+  handleMovingTimeChange = (e) => {
+    this.setState({ movingTime: e.target.value });
+  };
 
-        if (response.ok) {
-          console.log("Results posted successfully!");
-          this.setState({ redirectToFeat2: true });
-        } else {
-          console.error("Error posting results:", response.status);
+  handleConfirmClick = () => {
+    this.setState({ confirmedMovingTime: this.state.movingTime });
+    console.log("Confirmed Moving Time:", this.state.movingTime); // Log confirmed moving time
+  };
+
+  handleNextClick = async () => {
+    const { selectedLine, confirmedMovingTime } = this.state;
+    if (selectedLine) {
+      if (confirmedMovingTime) {
+        try {
+          // Post the results to the server
+          const response = await fetch("http://localhost:8081/results", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ result: selectedLine, movingTime: confirmedMovingTime }),
+          });
+
+          if (response.ok) {
+            console.log("Results posted successfully!");
+            this.setState({ redirectToFeat2: true });
+          } else {
+            console.error("Error posting results:", response.status);
+          }
+        } catch (error) {
+          console.error("Error posting results:", error);
         }
-      } catch (error) {
-        console.error("Error posting results:", error);
+      } else {
+        alert("Please enter and confirm the average moving time.");
       }
     } else {
       alert("Please select a line first.");
@@ -55,7 +70,7 @@ class PostResults extends Component {
   };
 
   render() {
-    const { redirectToFeat2, redirectToWillowdale, showModal } = this.state;
+    const { redirectToFeat2, redirectToWillowdale, showModal, movingTime, confirmedMovingTime } = this.state;
 
     if (redirectToFeat2) {
       return <Navigate to="/feat2" />;
@@ -72,11 +87,33 @@ class PostResults extends Component {
             <div style={{ display: "flex", justifyContent: "center" }}>
               <Card.Title>Results</Card.Title>
             </div>
-            {/* Render results here */}
-            <div style={{ display: "flex", justifyContent: "flex-end" }}>
-              <Button variant="danger" onClick={this.handleDiscardClick}>Discard</Button>
-              <Button onClick={this.handleNextClick}>Save</Button>
-            </div>
+            {confirmedMovingTime ? ( // Conditionally render form or confirmation
+              <div>
+                <strong>Moving Time:</strong> {confirmedMovingTime}
+              </div>
+            ) : (
+              <Form>
+                <Form.Group controlId="movingTime">
+                  <Form.Label>Enter The Total Moving Time</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={movingTime}
+                    onChange={this.handleMovingTimeChange}
+                  />
+                </Form.Group>
+              </Form>
+            )}
+            {!confirmedMovingTime && (
+              <Button variant="primary" onClick={this.handleConfirmClick}>
+                Confirm
+              </Button>
+            )}
+            {confirmedMovingTime ? null : ( // Hide buttons when confirmed
+              <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                <Button variant="danger" onClick={this.handleDiscardClick}>Discard</Button>
+                <Button onClick={this.handleNextClick}>Save</Button>
+              </div>
+            )}
           </Card.Body>
         </Card>
         {/* Alert Modal */}
