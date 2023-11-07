0d3faf901a2b9967784fca49928f2d00119e187a
Derek Martineau <derekmartineau@hotmail.com>
2023-11-07 18:54:13 -0500

    Frontend(Feat): initial Score Creation

--- frontend/src/components/pages/assessResultPage.js
+++ frontend/src/components/pages/assessResultPage.js
@@ -1,80 +1,120 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Card, Button, Modal, Form } from "react-bootstrap";
 import { Navigate, useParams } from "react-router-dom";
 
 function PostResults() {
-  const [data] = useState([]);
-  const [selectedLine, setSelectedLine] = useState(null);
   const [movingTime, setMovingTime] = useState("");
   const [confirmedMovingTime, setConfirmedMovingTime] = useState("");
   const [score, setScore] = useState(null);
-  const [activeButtonIndex] = useState(-1);
+  const [showModal, setShowModal] = useState(false);
+  const { assessmentId } = useParams();
   const [redirectToFeat2, setRedirectToFeat2] = useState(false);
   const [redirectToWillowdale, setRedirectToWillowdale] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+  const [featureLines, setFeatureLines] = useState([]); // Define featureLines in the component's state
+  const [selectedLine, setSelectedLine] = useState([]);
 
-const { assessmentId } = useParams();
-const [assessment, setAssessmentId] = useState(assessmentId);
+
+  const calculateScore = () => {
+    if (confirmedMovingTime && featureLines.length > 0) {
+      const movingTimeInSeconds = convertMovingTimeToSeconds(confirmedMovingTime);
+      console.log("Moving Time in Seconds:", movingTimeInSeconds);
   
-const handleMovingTimeChange = (e) => {
+      // Calculate the lineTotal by summing up the lineChoice values in featureLines
+      const lineTotal = featureLines.reduce((total, line) => total + parseInt(line.lineChoice, 10), 0);
+      console.log("Total Line Choice Score:", lineTotal);
+  
+      const maxMovingTime = 300; // You can adjust this value
+      const maxLineTotal = 48; // Maximum total for lineChoice
+  
+      // Calculate scores based on your criteria
+      const movingTimeScore = 100 - (movingTimeInSeconds / maxMovingTime) * 100;
+      console.log("Moving Time Score:", movingTimeScore);
+  
+      const lineTotalScore = (lineTotal / maxLineTotal) * 100;
+      console.log("Line Choice Score:", lineTotalScore);
+  
+      // Calculate the final score by summing the two scores
+      const finalScore = movingTimeScore + lineTotalScore;
+      setScore(finalScore.toFixed(2)); // Convert to a string with 2 decimal places
+      console.log("Calculated Score:", finalScore); // Log the final score here
+    }
+  };
+
+  useEffect(() => {
+    async function fetchAssessmentData() {
+      try {
+        const response = await fetch(`http://localhost:8081/api/results/${assessmentId}`);
+        if (response.ok) {
+          const assessmentData = await response.json();
+          if (assessmentData.featureLines) {
+            setFeatureLines(assessmentData.featureLines); // Set featureLines from the data
+            calculateScore(); // Calculate the score after setting featureLines
+          }
+        } else {
+          console.error("Error fetching assessment data:", response.status);
+        }
+      } catch (error) {
+        console.error("Error fetching assessment data:", error);
+      }
+    }
+    fetchAssessmentData();
+  }, [assessmentId, calculateScore]);
+  
+
+  const handleMovingTimeChange = (e) => {
     setMovingTime(e.target.value);
+  };
+
+  
+  const updateAssessment = async (score) => {
+    try {
+      const response = await fetch(`http://localhost:8081/api/results/${assessmentId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ Score: score }),
+      });
+
+      if (response.ok) {
+        console.log("Assessment updated successfully!");
+      } else {
+        console.error("Error updating assessment:", response.status);
+      }
+    } catch (error) {
+      console.error("Error updating assessment:", error);
+    }
+  };
+
+  const convertMovingTimeToSeconds = (time) => {
+    const [minutes, seconds] = time.split(":");
+    return parseInt(minutes, 10) * 60 + parseInt(seconds, 10);
   };
 
   const handleConfirmClick = () => {
     setConfirmedMovingTime(movingTime);
     console.log("Confirmed Moving Time:", movingTime);
-    calculateScore();
+    calculateScore(); // Calculate the score when the user confirms moving time.
   };
+  
 
-  const calculateScore = () => {
-    if (confirmedMovingTime && selectedLine) {
-      // Perform score calculation based on your criteria
-      const movingTimeInSeconds = convertMovingTimeToSeconds(confirmedMovingTime);
-      let selectedLineScore = selectedLine.reduce((acc, val) => acc + val, 0);
-      const maxMovingTime = 300; // You can adjust this value
-      const maxSelectedLineScore = 30; // You can adjust this value
-  
-      // Calculate scores based on your criteria
-      const movingTimeScore = (1 - movingTimeInSeconds / maxMovingTime) * 50;
-      selectedLineScore = (selectedLineScore / maxSelectedLineScore) * 50;
-  
-      // Calculate the final score and update the state
-      const finalScore = movingTimeScore + selectedLineScore;
-      setScore(finalScore.toFixed(2)); // Convert to a string with 2 decimal places
-    }
-  };
-  
-    const convertMovingTimeToSeconds = (time) => {
-      const [minutes, seconds] = time.split(":");
-      return parseInt(minutes, 10) * 60 + parseInt(seconds, 10);
-    };
-    
   const handleNextClick = async () => {
-    if (selectedLine) {
+    if (selectedLine.length > 0) {
       if (confirmedMovingTime) {
+        const scoreToSend = parseFloat(score);
         try {
-          const response = await fetch("http://localhost:8081/results", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ result: selectedLine, movingTime: confirmedMovingTime }),
-          });
+          // Update the assessment using the "updateAssessment" function
+          await updateAssessment(scoreToSend);
 
-          if (response.ok) {
-            console.log("Results posted successfully!");
-            setRedirectToFeat2(true);
-          } else {
-            console.error("Error posting results:", response.status);
-          }
+          // Set redirectToFeat2 to navigate to the next page
+          setRedirectToFeat2(true);
         } catch (error) {
-          console.error("Error posting results:", error);
+          console.error("Error updating assessment:", error);
+          // Handle the error, e.g., show an error message to the user.
         }
       } else {
         alert("Please enter and confirm the average moving time.");
       }
-    } else {
-      alert("Please select a line first.");
     }
   };
 
@@ -98,6 +138,10 @@ const handleMovingTimeChange = (e) => {
   if (redirectToWillowdale) {
     return <Navigate to="/willowdale" />;
   }
+  
+  
+  
+  
 
   return (
     <div style={{ background: "#5A5A5A", minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
@@ -118,19 +162,22 @@ const handleMovingTimeChange = (e) => {
               </Form.Group>
             </Form>
           )}
-          {!confirmedMovingTime && (
+          {!confirmedMovingTime ? (
             <Button variant="primary" onClick={handleConfirmClick}>
               Confirm
             </Button>
-          )}
-          {confirmedMovingTime ? null : (
-            <div style={{ display: "flex", justifyContent: "flex-end" }}>
-              <Button variant="danger" onClick={handleDiscardClick}>
-                Discard
-              </Button>
-              <Button onClick={handleNextClick}>Save</Button>
+          ) : null}
+          {score !== null && (
+            <div>
+              <strong>Score:</strong> {score}
             </div>
           )}
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <Button variant="danger" onClick={handleDiscardClick}>
+              Discard
+            </Button>
+            <Button onClick={handleNextClick}>Save</Button>
+          </div>
         </Card.Body>
       </Card>
       <Modal show={showModal} onHide={handleModalCancel}>
