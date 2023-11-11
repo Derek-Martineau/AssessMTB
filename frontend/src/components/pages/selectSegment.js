82f9179b5538ca8c37b913afebfab6ec1c3eefab
Derek Martineau <derekmartineau@hotmail.com>
2023-11-10 21:53:18 -0500

    frontend(chore) enhanced selectSegment ui

--- frontend/src/components/pages/selectSegment.js
+++ frontend/src/components/pages/selectSegment.js
@@ -16,7 +16,6 @@ const CallSegments = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Fetch data specific to the park using trailparkId
     const fetchData = async () => {
       try {
         const response = await fetch(`${process.env.REACT_APP_BACKEND_SERVER_URI}/parks/trailparks/getsegments/${trailparkId}`);
@@ -36,7 +35,6 @@ const CallSegments = () => {
 
   const handleNextClick = () => {
     if (selectedSegment) {
-      console.log("Selected Segment:", selectedSegment);
       setRedirectToFeat1(true);
     } else {
       alert("Please select a Segment first.");
@@ -44,19 +42,13 @@ const CallSegments = () => {
   };
 
   useEffect(() => {
-    console.log("useEffect for getUserInfo is running in CallSegments");
     const user = getUserInfo();
-    console.log("User Info:", user);
     setUser(user);
-  
-    // Check if redirectToFeat1 is true and user information is available
+
     if (redirectToFeat1 && user.userId && selectedSegment) {
       navigate(`/selectSegment/${selectedSegment._id}/${user.userId}/selectFeature`);
     }
   }, [redirectToFeat1]);
-  
-  
-  
 
   const handleBackClick = () => {
     setShowModal(true);
@@ -80,49 +72,51 @@ const CallSegments = () => {
   };
 
   if (redirectToFeat1) {
-    //return <Navigate to=`/selectSegment/:segmentId/${user.userId}/selectFeature` />;  
     navigate(`/selectSegment/${selectedSegment._id}/${user.id}/selectFeature`);
+    return null;
   }
+
   if (redirectToPreviousPage) {
     return <Navigate to="/newAssessment" />;
   }
 
   return (
     <div style={{ background: '#5A5A5A', minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-      <Card style={{ width: "18rem" }}>
+      <Card style={{ width: "30rem", borderRadius: "10px", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", background: '#f4f4f4' }}>
         <Card.Body>
-          <Card.Title>Which Segment Did You Ride?</Card.Title>
-          <div>
-            <ul>
-              {data.map(el => (
-                <li key={el.id}>
-                  <Button
-                    variant={selectedSegment === el ? "success" : "primary"}
-                    style={{ margin: "3px 0" }}
-                    onClick={() => handleSegmentSelection(el)}
-                  >
-                    {el.segmentName}
-                  </Button>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div style={{ display: "flex", justifyContent: "space-between", flexDirection: "row-reverse" }}>
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <Button variant="info" onClick={handleInstructionsShow}>Help</Button>
-              <Button variant="danger" onClick={handleBackClick}>Change Park</Button>
-              <Button variant="primary" onClick={handleNextClick} disabled={!selectedSegment}>
-                Next
+          <Card.Title style={{ fontSize: "1.5rem", marginBottom: "15px", textAlign: "center", color: "#333" }}>Which Segment Did You Ride?</Card.Title>
+          <div style={{ maxHeight: "300px", overflowY: "auto" }}>
+            {data.map(el => (
+              <Button
+                key={el.id}
+                variant={selectedSegment === el ? "success" : "primary"}
+                style={{
+                  margin: "5px 0",
+                  width: "100%",
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                }}
+                onClick={() => handleSegmentSelection(el)}
+              >
+                {el.segmentName}
               </Button>
-            </div>
+            ))}
+          </div>
+          <div style={{ display: "flex", justifyContent: "space-between", marginTop: "20px" }}>
+            <Button variant="info" onClick={handleInstructionsShow}>Help</Button>
+            <Button variant="danger" onClick={handleBackClick}>Change Park</Button>
+            <Button variant="primary" onClick={handleNextClick} disabled={!selectedSegment}>
+              Next
+            </Button>
           </div>
         </Card.Body>
       </Card>
       <Modal show={showModal} onHide={handleModalCancel}>
         <Modal.Header closeButton>
-          <Modal.Title>Change Park Confirmation</Modal.Title>
+          <Modal.Title style={{ color: "#333" }}>Change Park Confirmation</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
+        <Modal.Body style={{ color: "#555" }}>
           Are you sure you want to change the park?
         </Modal.Body>
         <Modal.Footer>
@@ -132,13 +126,13 @@ const CallSegments = () => {
       </Modal>
       <Modal show={showInstructionsModal} onHide={handleInstructionsClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Assessment Instructions</Modal.Title>
+          <Modal.Title style={{ color: "#333" }}>Assessment Instructions</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          <p><strong>Step 1:</strong> Select the park you would like to assess a segment in. Press the "Next" button to confirm and continue.</p>
-          <p><strong>Step 2:</strong> Select the segment you would like to assess your technical ability on.</p>
-          <p><strong>Step 3:</strong> Follow through the features displayed. Select the line that you traveled over. Lines are ranked in alphapbetical order descending. This means that the A line is the hardest line to complete and results in the most points possible per feature. You can also select walked if you had failed/missed the feature.</p>
-          <p><strong>Step 4:</strong> View your assessment score and decide to delete or share the segment. The Assessment score can also be saved to only your personal records if you wish not to share the results.</p>
+        <Modal.Body style={{ color: "#555" }}>
+          <p style={{ marginBottom: "10px" }}><strong>Step 1:</strong> Select the park you would like to assess a segment in. Press the "Next" button to confirm and continue.</p>
+          <p style={{ marginBottom: "10px" }}><strong>Step 2:</strong> Select the segment you would like to assess your technical ability on.</p>
+          <p style={{ marginBottom: "10px" }}><strong>Step 3:</strong> Follow through the features displayed. Select the line that you traveled over. Lines are ranked in alphabetical order descending. This means that the A line is the hardest line to complete and results in the most points possible per feature. You can also select walked if you had failed/missed the feature.</p>
+          <p style={{ marginBottom: "10px" }}><strong>Step 4:</strong> View your assessment score and decide to delete or share the segment. The Assessment score can also be saved to only your personal records if you wish not to share the results.</p>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleInstructionsClose}>Close</Button>
