8426244d4f9df2d8417806c6e4b1d5b253975b22
Derek Martineau <derekmartineau@hotmail.com>
2023-12-31 15:14:36 -0500

    frontend(chore): redesigned explore page

--- frontend/src/components/pages/feedPage/explore.js
+++ frontend/src/components/pages/feedPage/explore.js
@@ -1,9 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './explore.css';
 
 const PublicAssessments = () => {
   const [publicAssessments, setPublicAssessments] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const fetchUsername = async (userId) => {
+    try {
+      if (!userId) {
+        throw new Error('Invalid user ID');
+      }
+  
+      const response = await axios.get(`${process.env.REACT_APP_BACKEND_SERVER_URI}/users/${userId}`);
+      return response.data.username;
+    } catch (error) {
+      console.error(`Error fetching username for user ID ${userId}:`, error);
+      return null;
+    }
+  };
+  
 
   useEffect(() => {
     const fetchPublicAssessments = async () => {
@@ -20,21 +36,11 @@ const PublicAssessments = () => {
             }
     
             try {
-              const userResponse = await axios.get(`${process.env.REACT_APP_BACKEND_SERVER_URI}/api/users/${assessment.User}`);
-              if (userResponse.data) {
-                const userData = userResponse.data;
-                assessment.username = userData.username;
-              }
+              // Fetch username using the user ID
+              const username = await fetchUsername(assessment.User);
+              assessment.username = username || 'User Not Found';
             } catch (userError) {
               console.error(`Error fetching user details for user ID ${assessment.User}:`, userError);
-    
-              // Handle 404 error more gracefully
-              if (userError.response && userError.response.status === 404) {
-                // User not found, you can set a default username or handle it as appropriate
-                assessment.username = 'User Not Found';
-              } else {
-                // Handle other errors as needed
-              }
             }
     
             return assessment;
@@ -53,24 +59,33 @@ const PublicAssessments = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Public Assessments</h1>
-      <div className="assessments-container">
+    <div className="feedContainer">
+      <h1 className="heading">Welcome To The Explore Page!</h1>
+      <div className="assessmentsContainer">
         {loading ? (
-          <p>Loading...</p>
+          <div className="loadingContainer">
+            Loading...
+          </div>
         ) : (
           publicAssessments.length > 0 ? (
             publicAssessments.map((assessment) => (
-              <div key={assessment._id} className="assessment-card">
-                <h2>Assessment Date: {new Date(assessment.Date).toDateString()}</h2>
-                <p>User: {assessment.username}</p>
+              <div key={assessment._id} className="assessmentCard">
+                <div className="userInfo">
+                  <img
+                    src={`https://robohash.org/${assessment.username}?set=set5`}
+                    alt="User Avatar"
+                    style={{ width: '150px', height: '150px', borderRadius: '50%' }}
+                  />
+                  <p className="username">{assessment.username}</p>
+                </div>
+                <h2 className="cardTitle">Assessment Date: {new Date(assessment.Date).toDateString()}</h2>
                 <p>Segment: {assessment.segmentName}</p>
                 <p>Difficulty: {assessment.difficulty}</p>
                 <p>Line Choices: {assessment.featureLines.map((line) => line.lineChoice).join(', ')}</p>
                 <p>Score: {assessment.Score}</p>
               </div>
             ))) : (
-              <p>No public assessments found.</p>
+              <p className="emptyState">No public assessments found.</p>
             )
         )}
       </div>
