fdf33c4f8ee5f8decec3f4f42a3ec5101f7d30d3
Derek Martineau <derekmartineau@hotmail.com>
2024-02-29 21:45:24 -0500

    frontend(chore) redesigned Assessment Library

--- frontend/src/components/pages/user/assessmentLibrary.js
+++ frontend/src/components/pages/user/assessmentLibrary.js
@@ -4,12 +4,11 @@ import { useParams } from 'react-router-dom';
 
 const ViewLibrary = () => {
   const [assessments, setAssessments] = useState([]);
-  const [showDeleteModal, setShowDeleteModal] = useState(false); // State to control the modal
-  const [assessmentToDelete, setAssessmentToDelete] = useState(null); // State to store the assessment to delete
-  const { username } = useParams(); // Get the username from the URL
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [assessmentToDelete, setAssessmentToDelete] = useState(null);
+  const { username } = useParams();
 
   useEffect(() => {
-    // Use the 'username' variable to customize the API route
     fetch(`${process.env.REACT_APP_BACKEND_SERVER_URI}/api/results/user/${username}`)
       .then((response) => {
         if (!response.ok) {
@@ -18,11 +17,9 @@ const ViewLibrary = () => {
         return response.json();
       })
       .then(async (data) => {
-        // Create an array to store the updated assessments
         const updatedAssessments = [];
 
         for (const assessment of data) {
-          // Fetch the Segment data for each assessment
           const segmentResponse = await fetch(`${process.env.REACT_APP_BACKEND_SERVER_URI}/api/segments/${assessment.Segment}`);
           if (segmentResponse.ok) {
             const segmentData = await segmentResponse.json();
@@ -48,13 +45,11 @@ const ViewLibrary = () => {
 
   const confirmDelete = () => {
     if (assessmentToDelete) {
-      // Send a request to your backend API to delete the assessment
       fetch(`${process.env.REACT_APP_BACKEND_SERVER_URI}/api/results/${assessmentToDelete}`, {
         method: 'DELETE',
       })
         .then((response) => {
           if (response.ok) {
-            // If the delete request is successful, update the UI by filtering out the deleted assessment
             setAssessments((currentAssessments) =>
               currentAssessments.filter((assessment) => assessment._id !== assessmentToDelete)
             );
@@ -75,8 +70,7 @@ const ViewLibrary = () => {
   };
 
   const handleSetPublic = (assessmentId, currentPublicStatus) => {
-    // Send a request to your backend API to update the PublicStatus
-    const newPublicStatus = !currentPublicStatus; // Toggle the PublicStatus
+    const newPublicStatus = !currentPublicStatus;
     fetch(`${process.env.REACT_APP_BACKEND_SERVER_URI}/api/results/${assessmentId}`, {
       method: 'PUT',
       headers: {
@@ -86,7 +80,6 @@ const ViewLibrary = () => {
     })
       .then((response) => {
         if (response.ok) {
-          // If the update request is successful, update the UI by setting the PublicStatus
           setAssessments((currentAssessments) =>
             currentAssessments.map((assessment) =>
               assessment._id === assessmentId ? { ...assessment, PublicStatus: newPublicStatus } : assessment
@@ -101,61 +94,55 @@ const ViewLibrary = () => {
       });
   };
 
-// Function to map numbers to letters
-const mapNumberToLetter = (number) => {
-  // Parse the number as an integer
-  const parsedNumber = parseInt(number, 10);
+  const mapNumberToLetter = (number) => {
+    const parsedNumber = parseInt(number, 10);
 
-  switch (parsedNumber) {
-    case 8:
-      return 'A';
-    case 6:
-      return 'B';
-    case 4:
-      return 'C';
-    case 1:
-      return 'Walked';
-    default:
-      return '';
-  }
-};
+    switch (parsedNumber) {
+      case 8:
+        return 'A';
+      case 6:
+        return 'B';
+      case 4:
+        return 'C';
+      case 1:
+        return 'Walked';
+      default:
+        return '';
+    }
+  };
 
   return (
-    <div style={{ background: '#5A5A5A', minHeight: '100vh', overflowX: 'hidden' }}>
-      <h1 style={{ textAlign: 'center', color: 'white' }}>Assessment Library</h1>
-      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', fontFamily: "sans-serif", fontSize: "18px" }}>
+    <div style={{ minHeight: '100vh', overflowX: 'hidden', fontFamily: 'Arial, sans-serif', color: '#333' }}>
+      <h1 style={{ textAlign: 'center', color: '#333', fontSize: '3rem', margin: '20px 0', textShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>Assessment Library</h1>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
         {assessments.length > 0 ? (
-          assessments.map((assessment) => (
+          assessments.reverse().map((assessment) => (
             <div key={assessment._id} style={assessmentCardStyle}>
-              <h2>Assessment Date: {new Date(assessment.Date).toDateString()}</h2>
-              <p>User: {username}</p>
-              <p>Segment: {assessment.segmentName}</p>
-              <p>Public Status: {assessment.PublicStatus ? 'Public' : 'Private'}</p>
-              <p>Difficulty: {assessment.difficulty}</p>
-              <p>
-              Line Choices: {assessment.featureLines.map((line) => {
-                console.log('line', line); // Add this console log
-                const letter = mapNumberToLetter(line.lineChoice);
-                console.log('letter', letter); // Add this console log
-                return letter;
-              }).join(', ')}
-            </p>
-              <p>Score: {assessment.Score}</p>
-              <div style={{ display: 'flex', justifyContent: 'space-between', padding: '0px' }}>
-                <Button variant="danger" onClick={() => handleDeleteAssessment(assessment._id)}>Delete</Button>
-                 <div style={{ marginLeft: '10px' }}>
-                 <Button
-                    variant={assessment.PublicStatus ? "warning" : "warning"}
-                    onClick={() => handleSetPublic(assessment._id, assessment.PublicStatus)}
-                    >
-                    {assessment.PublicStatus ? "Set Private" : "Set Public"}
-                </Button>
-                </div>
-              </div>
-            </div>
+  <div style={sideBySideContainer}>
+    <p style={{ fontSize: '1.4rem', fontWeight: 'bold', margin: '0', color: '#222' }}>User: {username}</p>
+    <p style={{ fontSize: '1.4rem', fontWeight: 'bold', margin: '0', color: '#222' }}>Score: {assessment.Score}</p>
+  </div>
+    <p style={{ fontSize: '1.2rem', color: '#555', borderBottom: '1px solid #000000', width: '100%' }}>Created: {new Date(assessment.Date).toDateString()}</p>
+  <p style={{ fontSize: '1.2rem',  color: '#000000' }}>Line Choices: {assessment.featureLines.map((line, index) => mapNumberToLetter(line.lineChoice)).join(', ')}</p>
+  <p style={{ fontSize: '1.2rem',  color: '#000000' }}>Segment: {assessment.segmentName}</p>
+  <p style={{ fontSize: '1.2rem',  color: '#000000' }}>Difficulty: {assessment.difficulty}</p>
+  <p style={{ fontSize: '1.2rem', margin: '0px 0', color: '#000000', borderTop: '1px solid #000000', width: '100%' }}>Public Status: {assessment.PublicStatus ? 'Public' : 'Private'}</p>
+  <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
+    <Button variant="danger" style={{ fontSize: '1.2rem' }} onClick={() => handleDeleteAssessment(assessment._id)}>Delete</Button>
+    <div style={{ marginLeft: '10px' }}>
+      <Button
+        variant={assessment.PublicStatus ? 'warning' : 'success'}
+        style={{ fontSize: '1.2rem' }}
+        onClick={() => handleSetPublic(assessment._id, assessment.PublicStatus)}
+      >
+        {assessment.PublicStatus ? "Set Private" : "Set Public"}
+      </Button>
+    </div>
+  </div>
+</div>
           ))
         ) : (
-          <p>No assessments found for the specified user.</p>
+          <p style={{ fontSize: '1.6rem', textAlign: 'center', color: '#555' }}>No assessments found for the specified user.</p>
         )}
       </div>
 
@@ -180,18 +167,24 @@ const mapNumberToLetter = (number) => {
 };
 
 const assessmentCardStyle = {
-  width: '300px',
-  minHeight: '200px',
+  width: '100%',
+  maxWidth: '450px',
   padding: '20px',
   margin: '20px',
-  background: 'white',
+  background: '#f7f7f7',
   borderRadius: '10px',
-  boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
   display: 'flex',
   flexDirection: 'column',
-  alignItems: 'center',
+  alignItems: 'flex-start',
   justifyContent: 'space-between',
-  textAlign: 'center',
+  textAlign: 'left',
+};
+
+const sideBySideContainer = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  width: '100%',
 };
 
 export default ViewLibrary;
